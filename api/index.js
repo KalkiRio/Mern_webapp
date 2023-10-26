e9cca7bd4ba01b7924d3d18dc9a022882bdabe80
KalkiRio <ribhuroy@outlook.com>
2023-10-26 22:43:12 +0530

    env updation

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+mongoose.connect()
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000!");
+    console.log("server is running on port 3000!" );
     }
 );
